17c212de508944bd940140c4f7c1c4a23f78ee9f
Pham Nam <zvw180ptn1411@gmail.com>
2025-11-01 10:41:27 +0700

    call

--- public/api/auth/index.php
+++ public/api/auth/index.php
@@ -1,48 +1,22 @@
 <?php
 
 /**
- * Simple GitHub OAuth Proxy for DecapCMS
+ * GitHub OAuth Start
  * Author: Phạm Thành Nam
  */
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, POST");
-header("Content-Type: application/json");
-
 $client_id = "Ov23liD9t7YNfpf1tS1f";
-$client_secret = "524305df09b2dda8feed354ef6676e9fb8e546c9";
-$redirect_uri = "https://server-message.bug.edu.vn/admin/";
+$redirect_uri = "https://server-message.bug.edu.vn/api/auth/callback.php";
+$scope = "repo,user";
 
-// Khi DecapCMS gọi /api/auth?code=... GitHub sẽ gửi code về đây
-if (isset($_GET['code'])) {
-  $code = $_GET['code'];
+// Tạo URL đăng nhập GitHub OAuth
+$auth_url = sprintf(
+  "https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s&scope=%s",
+  urlencode($client_id),
+  urlencode($redirect_uri),
+  urlencode($scope)
+);
 
-  $postdata = [
-    "client_id" => $client_id,
-    "client_secret" => $client_secret,
-    "code" => $code,
-    "redirect_uri" => $redirect_uri,
-  ];
-
-  $opts = [
-    "http" => [
-      "method"  => "POST",
-      "header"  => "Content-Type: application/json\r\nAccept: application/json\r\n",
-      "content" => json_encode($postdata),
-    ],
-  ];
-
-  $context = stream_context_create($opts);
-  $result = file_get_contents("https://github.com/login/oauth/access_token", false, $context);
-
-  if ($result === FALSE) {
-    echo json_encode(["error" => "Failed to get access token"]);
-    exit;
-  }
-
-  echo $result;
-  exit;
-}
-
-echo json_encode(["error" => "Missing ?code parameter"]);
+// Chuyển hướng người dùng tới GitHub
+header("Location: $auth_url");
+exit;
